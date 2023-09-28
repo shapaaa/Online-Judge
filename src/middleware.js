f8aacbc8920b70d96fc9141c9da7582e816ca5e9
Shardul Pathak <48183855+shapaaa@users.noreply.github.com>
2023-09-28 20:25:28 +0530

    Update middleware to handle login and register routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,21 @@
-import { NextResponse } from 'next/server';
-import { jwtVerify } from 'jose';
+import { NextResponse } from "next/server";
+import verifyJwt from "./app/services/verifyJwt";
 export const config = {
-	matcher: '/api/:function*',
+  matcher: "/api/:function*",
 };
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
-	const { value: token } = request.cookies?.get('jwt');
-	const { pathname } = request.nextUrl;
-	if (pathname == '/api/login' && !token) {
-		return NextResponse.next();
-	}
-	try {
-		const { payload } = await jwtVerify(token, new TextEncoder().encode(process.env.SECRET_KEY));
-		return NextResponse.next();
-	} catch (error) {
-		return NextResponse.json(error.message);
-	}
+  const jwt = request.cookies?.get("jwt");
+  const { pathname } = request.nextUrl;
+
+  if ((pathname == "/api/login" || pathname == "/api/register") && !jwt) {
+    return NextResponse.next();
+  }
+  try {
+    const { value: token } = jwt;
+    const result = await verifyJwt(token);
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.error(error.message);
+  }
 }
