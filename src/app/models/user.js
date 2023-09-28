412b460c8ccb6da69293d3971e9e5434e5289021
Shardul Pathak <48183855+shapaaa@users.noreply.github.com>
2023-09-28 20:24:43 +0530

    Update user model

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,23 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const UserSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		unique: true,
-		max: 20,
-	},
-	email: {
-		type: String,
-		required: true,
-		max: 100,
-	},
-	password: { type: String, required: true },
-	verified: {
-		type: Boolean,
-		default: false,
-	},
-	createdat: { type: Date, default: Date.now() },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 100,
+    unique: true,
+  },
+  password: { type: String, required: true },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  createdat: { type: Date, default: Date.now() },
 });
-const User = mongoose.models.user || mongoose.model('user', UserSchema);
+const User = mongoose.models.user || mongoose.model("user", UserSchema);
 export default User;
