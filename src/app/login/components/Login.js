810e17d6eb62dba929ad1f6372331b93e4549772
Shardul Pathak <shardul.pathak@agiliad.com>
2023-10-26 13:20:02 +0530

    Show loading  spinner for login process

--- src/app/login/components/Login.js
+++ src/app/login/components/Login.js
@@ -1,4 +1,5 @@
 "use client";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -7,7 +8,8 @@ import { useState } from "react";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { push, refresh } = useRouter();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const handleEmail = (e) => {
     setEmail(e.target.value);
   };
@@ -16,99 +18,105 @@ const Login = () => {
   };
   const handleClick = async () => {
     try {
+      setLoading(true);
       const result = await axios.post("/api/login", { email, password });
-      refresh();
-      push("/");
+      router.refresh();
+      router.push("/");
     } catch (error) {
       console.log(error);
     }
+    setLoading(false);
   };
   return (
     <section>
-      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 ">
-        <div className="w-full rounded-lg bg-white shadow  sm:max-w-md md:mt-0 xl:p-0">
-          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900  md:text-2xl">
-              Sign in to your account
-            </h1>
-            <div className="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-900 "
-                >
-                  Email address
-                </label>
-                <input
-                  onChange={handleEmail}
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
-                  placeholder="E-mail address"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-900 "
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={handlePassword}
-                  placeholder="••••••••"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
-                  required=""
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex h-5 items-center">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="focus:ring-3 focus:ring-primary-300 h-4 w-4 rounded border border-gray-300 bg-gray-50 "
-                      required=""
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-gray-500 ">
-                      Remember me
-                    </label>
-                  </div>
+      <div className=" flex flex-col items-center justify-center px-6 py-8 ">
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <div className="w-full rounded-lg bg-white shadow  sm:max-w-md md:mt-0 xl:p-0">
+            <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900  md:text-2xl">
+                Sign in to your account
+              </h1>
+              <div className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block text-sm font-medium text-gray-900 "
+                  >
+                    Email address
+                  </label>
+                  <input
+                    onChange={handleEmail}
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+                    placeholder="E-mail address"
+                    required=""
+                  />
                 </div>
-                <Link
-                  href="#"
-                  className="text-primary-600 text-sm font-medium hover:underline"
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="mb-2 block text-sm font-medium text-gray-900 "
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={handlePassword}
+                    placeholder="••••••••"
+                    className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+                    required=""
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex h-5 items-center">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="focus:ring-3 focus:ring-primary-300 h-4 w-4 rounded border border-gray-300 bg-gray-50 "
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label htmlFor="remember" className="text-gray-500 ">
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <Link
+                    href="#"
+                    className="text-primary-600 text-sm font-medium hover:underline"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
+                <button
+                  onClick={handleClick}
+                  type="submit"
+                  className="focus:ring-primary-300 w-full rounded-lg bg-slate-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-slate-800 focus:outline-none focus:ring-4"
                 >
-                  Forgot password?
-                </Link>
+                  Sign in
+                </button>
+                <p className="text-sm font-light text-gray-500 ">
+                  Don’t have an account yet?{" "}
+                  <Link
+                    href="/register"
+                    className="text-primary-600  font-medium hover:underline"
+                  >
+                    Sign up
+                  </Link>
+                </p>
               </div>
-              <button
-                onClick={handleClick}
-                type="submit"
-                className="focus:ring-primary-300 w-full rounded-lg bg-slate-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-slate-800 focus:outline-none focus:ring-4"
-              >
-                Sign in
-              </button>
-              <p className="text-sm font-light text-gray-500 ">
-                Don’t have an account yet?{" "}
-                <Link
-                  href="/register"
-                  className="text-primary-600  font-medium hover:underline"
-                >
-                  Sign up
-                </Link>
-              </p>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
