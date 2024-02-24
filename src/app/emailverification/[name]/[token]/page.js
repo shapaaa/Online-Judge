1a545fa11809145a0784bde90d48670c114e8719
Shardul Pathak <48183855+shapaaa@users.noreply.github.com>
2024-02-24 22:19:51 +0530

    Use correct component name

--- src/app/emailverification/[name]/[token]/page.js
+++ src/app/emailverification/[name]/[token]/page.js
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 import { useEffect } from "react";
-export default function verifyUserEmail({ params: userInfo }) {
+export default function EmailVerificationPage({ params: userInfo }) {
   const [message, setMessage] = useState("");
   useEffect(() => {
     const verifyEmailToken = async () => {
